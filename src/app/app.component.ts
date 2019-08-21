9b8187ac0ff11f682fe35b639d66fb519bfb2656
Dalton Dayton <daltondayton1@gmail.com>
2019-08-20 20:12:46 -0600

    'Prettier' formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+    serverElements = [];
+    newServerName = '';
+    newServerContent = '';
 
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
+    onAddServer() {
+        this.serverElements.push({
+            type: 'server',
+            name: this.newServerName,
+            content: this.newServerContent,
+        });
+    }
 
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
+    onAddBlueprint() {
+        this.serverElements.push({
+            type: 'blueprint',
+            name: this.newServerName,
+            content: this.newServerContent,
+        });
+    }
 }
