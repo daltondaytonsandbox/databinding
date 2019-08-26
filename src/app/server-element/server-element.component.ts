391eddeff648ba39c9eb9a4ffeeea3b2b5fd9931
Dalton Dayton <daltondayton1@gmail.com>
2019-08-26 01:26:42 -0600

    Add Lifecycle functions/notes to server.ts

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    Input,
+    ViewEncapsulation,
+    OnChanges,
+    SimpleChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy,
+} from '@angular/core';
 
 @Component({
     selector: 'app-server-element',
@@ -6,10 +19,79 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
     styleUrls: ['./server-element.component.css'],
     encapsulation: ViewEncapsulation.Emulated, // Emulated is default. This line isn't needed.
 })
-export class ServerElementComponent implements OnInit {
-    @Input() element: { type: string; name: string; content: string };
+export class ServerElementComponent
+    implements
+        OnInit,
+        OnChanges,
+        DoCheck,
+        AfterContentInit,
+        AfterContentChecked,
+        AfterViewInit,
+        AfterViewChecked,
+        OnDestroy {
+    // @Input() element: { type: string; name: string; content: string };
+    @Input() name: string;
 
-    constructor() {}
+    constructor() {
+        console.log('Constructor Called!');
+    }
 
-    ngOnInit() {}
+    // 'Lifecycle'
+    // 'ngOnChanges' - Called after a bound input property changes
+    // 'ngOnInit' - Called once the component is initialized
+    // 'ngDoCheck' - Called during every change detection run
+    // 'ngAfterContentInit' - Called after content (ng-content) has been projected into view
+    // 'ngAfterContentChecked' - Called every time the projected content has been checked
+    // 'ngAfterViewInit' - Called after the component's view and (child views) has been initialzed
+    // 'ngAfterViewChecked' - Called every time the view (and child views) have been checked
+
+    ngOnChanges(changes: SimpleChanges) {
+        console.log('ngOnChanges Called!');
+        console.log(changes);
+        // Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+        // Add '${implements OnChanges}' to the class.
+    }
+
+    ngOnInit() {
+        console.log('ngOnInit Called!');
+        // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        // Add 'implements OnInit' to the class.
+    }
+
+    ngDoCheck() {
+        console.log('ngDoCheck Called!');
+        // Called every time that the input properties of a component or a directive are checked.
+        // Use it to extend change detection by performing a custom check.
+        // Add 'implements DoCheck' to the class.
+    }
+
+    ngAfterContentInit() {
+        console.log('ngAfterContentInit Called!');
+        // Called after ngOnInit when the component's or directive's content has been initialized.
+        // Add 'implements AfterContentInit' to the class.
+    }
+
+    ngAfterContentChecked() {
+        console.log('ngAfterContentChecked Called!');
+        // Called after every check of the component's or directive's content.
+        // Add 'implements AfterContentChecked' to the class.
+    }
+
+    ngAfterViewInit() {
+        console.log('ngAfterViewInit Called!');
+        // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+        // Add 'implements AfterViewInit' to the class.
+    }
+
+    ngAfterViewChecked() {
+        console.log('ngAfterViewChecked Called!');
+        // Called after every check of the component's view. Applies to components only.
+        // Add 'implements AfterViewChecked' to the class.
+    }
+
+    ngOnDestroy(): void {
+        console.log('ngOnDestroy Called!');
+        // Called once, before the instance is destroyed.
+        // Add 'implements OnDestroy' to the class.
+    }
 }
