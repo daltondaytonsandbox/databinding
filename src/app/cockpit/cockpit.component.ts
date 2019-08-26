1ad410a4be4a66a36e2e7f3c75b4a9a0377cb45c
Dalton Dayton <daltondayton1@gmail.com>
2019-08-25 19:57:24 -0600

    Add Component Lifecycle notes

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -33,6 +33,15 @@ export class CockpitComponent implements OnInit {
 
     constructor() {}
 
+    // 'Lifecycle'
+    // 'ngOnChanges' - Called after a bound input property changes
+    // 'ngOnInit' - Called once the component is initialized
+    // 'ngDoCheck' - Called during every change detection run
+    // 'ngAfterContentInit' - Called after content (ng-content) has been projected into view
+    // 'ngAfterContentChecked' - Called every time the projected content has been checked
+    // 'ngAfterViewInit' - Called after the component's view and (child views) has been initialzed
+    // 'ngAfterViewChecked' - Called every time the view (and child views) have been checked
+
     ngOnInit() {}
 
     onAddServer(nameInput: HTMLInputElement) {
